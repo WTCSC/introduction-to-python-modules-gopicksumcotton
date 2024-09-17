06bae41ee734b3d5da55f8aa7be34b7ec1968726
Dacio <dvedor466@gmail.com>
2024-09-17 14:36:36 -0600

    Pythonscool

--- average.py
+++ average.py
@@ -10,3 +10,37 @@ format:
 
 "Average words per line: [average]"
 """
+import argparse
+import text_utils
+def averagewords_perline(filename):
+  """
+  Reference the "Reading Files in Python" lesson to open the `file.txt` file, count the number of words in the file, and return the count:
+  """
+  #open the file 
+  file = open(filename, 'r')
+  #read all lines into a list
+  lines = file.readlines()
+  x=0 
+  water=0
+  #itterate over each line 
+  for line in lines:
+    x=x+1 
+    water = water+text_utils.count_words(line)
+  
+  #close the file
+  file.close()
+  return water // x
+
+def main():
+  parser = argparse.ArgumentParser(description='count lines in file.')
+  parser.add_argument('filename', help='The file to count lines from')
+  args = parser.parse_args()
+
+
+  count = averagewords_perline(args.filename)
+  print(f"Average words per line: {count}")
+ 
+
+if __name__ =='__main__':
+  main()
+
